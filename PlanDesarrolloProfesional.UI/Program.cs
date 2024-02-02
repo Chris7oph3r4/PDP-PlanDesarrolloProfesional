6466f80be734ab8d9749356dee8e44d6b1d7a1de
Christopher Araya <caraya@datasoftla.com>
2024-02-01 19:50:06 -0600

    Program

--- PlanDesarrolloProfesional.UI/Program.cs
+++ PlanDesarrolloProfesional.UI/Program.cs
@@ -1,9 +1,15 @@
+using Microsoft.AspNetCore.Mvc.Filters;
+using PlanDesarrolloProfesional.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+IWebHostEnvironment env = app.Environment;
+var env1 = env.WebRootPath;
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -17,6 +23,20 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+#region Appsettings
+
+var config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, true)
+    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, true)
+    .Build();
+StringConexion.ConexionSQL = builder.Configuration.GetConnectionString("SqlConnection"); //cuando se actualiza el contexto, hay que revisar la cadena de conexion del PlanDesarrolloProfesionalContext, ubicado en capa Models
+
+#endregion Appsettings
+
+
+
 
 app.UseAuthorization();
 
