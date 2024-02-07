b3df787a124b4fc0952d0fff47d26791e4f9aabe
Christopher Araya <caraya@datasoftla.com>
2024-02-07 15:09:03 -0600

    Configuraciones para Login y Claims

---
+++ PlanDesarrolloProfesional.Models/Models/Usuario/UsuarioModel.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanDesarrolloProfesional.Models.Models
+{
+    public class UsuarioModel
+    {
+        [Key]
+        public int UsuarioID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Unicode(false)]
+        public string Nombre { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        [Unicode(false)]
+        public string Descripcion { get; set; }
+
+        public int RolID { get; set; }
+
+        public int AreaID { get; set; }
+
+        public int JerarquiaID { get; set; }
+
+        public Guid CodigoDaloo { get; set; }
+    }
+}
--- PlanDesarrolloProfesional.UI/Controllers/HomeController.cs
+++ PlanDesarrolloProfesional.UI/Controllers/HomeController.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PlanDesarrolloProfesional.Models.Models;
 using PlanDesarrolloProfesional.UI.Models;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace PlanDesarrolloProfesional.UI.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -15,12 +20,50 @@ namespace PlanDesarrolloProfesional.UI.Controllers
 
         public IActionResult Index()
         {
+            var claimsPrincipal = HttpContext.User as ClaimsPrincipal;
+
+            // Acceder a los claims del usuario
+            var userIdClaim = claimsPrincipal?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var userNameClaim = claimsPrincipal?.FindFirst(ClaimTypes.Name)?.Value;
+       
+            UsuarioViewModel Usuario = new UsuarioViewModel();//Obtener usuario por correo  
+            Usuario.Rol = "Gerente";
+
+            var existingRoleClaim = claimsPrincipal?.FindFirst(ClaimTypes.Role);
+
+            // Si no tiene un claim de rol, agregar uno
+            if (existingRoleClaim == null)
+            {
+                // Crear un nuevo claim de rol
+                var newRoleClaim = new Claim(ClaimTypes.Role, Usuario.Rol.ToString());
+
+                // Crear un nuevo ClaimsIdentity y agregar el nuevo claim de rol
+                var claimsIdentity = new ClaimsIdentity(claimsPrincipal.Identity);
+                claimsIdentity.AddClaim(newRoleClaim);
+
+                // Crear un nuevo ClaimsPrincipal con el ClaimsIdentity actualizado
+                var newClaimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+
+                // Actualizar el ClaimsPrincipal en el contexto actual
+                HttpContext.User = newClaimsPrincipal;
+            }
+
+            return View();
+        }
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
             return View();
         }
 
         public IActionResult Privacy()
         {
             return View();
+        }
+        // Acción para realizar el logout
+        public IActionResult Logout()
+        {
+            return SignOut("Cookies", "OpenIdConnect");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
